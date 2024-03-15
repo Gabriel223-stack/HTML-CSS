34d43fefb135ecca70d4482daf73bddc8b02608d
Gabriel Lopes <125931614+Gabriel223-stack@users.noreply.github.com>
2024-03-15 19:50:08 -0300

    menu de interação

---
+++ exercicios/ex026/mq005/script.js
@@ -0,0 +1,7 @@
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none' // SE O ITENS MOSTRAR VAI SE ESCONDIDO POR CAUSA DO NONE
+    } else {
+        itens.style.display = 'block' // APARECE
+    }
+}
